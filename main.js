5f11419f3e366bd26312caf37ca6aceaa3fa38c8
Arezoo <arezooayoblo@gmail.com>
2024-09-26 23:28:10 +0330

    second way

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-let currentAudio=null;
-function playSound(soundId){
-    if(currentAudio){
-        currentAudio.pause();
-        currentAudio.currentTime=0;
-    }
-    const audio=document.getElementById(soundId)
-    if(audio){
-        audio.play();
-        currentAudio=audio
-    }
-}
+// let currentAudio=null;
+// function playSound(soundId){
+//     if(currentAudio){
+//         currentAudio.pause();
+        
+//     }
+//     const audio=document.getElementById(soundId)
+//     if(audio){
+//         audio.play();
+//         currentAudio=audio
+//     }
+// }
 
+const buttons=document.querySelectorAll('.sound')
+let currentAudio=null
+buttons.forEach(button=> {
+    button.addEventListener('click',()=>{
+        const  soundButton=button.getAttribute("sound");
+        const audios=document.querySelector(`audio[id=${soundButton}]`);
+        if (currentAudio){
+            currentAudio.pause()
+        }
+        audios.play();
+        currentAudio=audios;
+
+    })
+    
+})
 
